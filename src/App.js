60078d135736e5f300d697aa9c3270ffb107cf36
Sahil3M3 <sahil3m3@gmail.com>
2024-12-22 18:26:31 +0530

    Cart Context Created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header"
 import Meals from "./components/Meals/Meals";
-
+import CartProvider from "./store/CartProvider"
 
 function App() {
 
@@ -16,13 +16,13 @@ const hideCartHandler=()=>{
   setCartIsShown(false);
 }
   return (
-    <>
+    <CartProvider>
     { cartIsShown && <Cart  onClose={hideCartHandler} />}
     <Header onShowCart={showCartHandler}/>
     <main>
       <Meals/>
     </main>
-    </>
+    </CartProvider>
   );
 }
 
